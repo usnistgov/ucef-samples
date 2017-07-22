bed11c6d6809a13755eb75ea4720e3701a1a5e0c
MartyBurns <marty@hypertek.us>
2017-07-22 14:48:15 -0400

    initial refactor of fedmanager and injection federate for new VM -- not fully functioning yet

--- UCEFWorkshopExercise/src/fedmanager/run.sh
+++ UCEFWorkshopExercise/src/fedmanager/run.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
 RTI_RID_FILE=RTI.rid
-federate_name=FedManager
-DIR=`pwd`
-cp fedmgrconfig.yml script.xml FedManager.fed target/
-cd target
-java \
--Djava.library.path=nar/processid-0.3.0-SNAPSHOT-amd64-Linux-gpp-jni/lib/amd64-Linux-gpp/jni/ \
--Djava.net.preferIPv4Stack=true \
--jar fedmanager-inject-0.0.1-SNAPSHOT.jar \
-server \
-fedmgrconfig.yml
-
-echo "DIR=$DIR pwd="`pwd`  
+mvn exec:java -X -P FederationManagerExecJava
 #cd $DIR
