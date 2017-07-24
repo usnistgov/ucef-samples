7403745c6a5b8c5bf7be5ec62e967bc13eaa2127
Thomas Roth <thomas.roth@nist.gov>
2017-07-24 19:00:19 +0000

    changed random house names to use GUID

--- UCEFWorkshopExercise/runahouse.sh
+++ UCEFWorkshopExercise/runahouse.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-# Run the house federate with either a random name or command line provided name
-# this script must be run from the workshopexercise folder
-
 DIR=`pwd`
 
 #use this selection to produce log files of all terminal content
@@ -10,20 +6,16 @@ logtofile="-l "
 #logtofile=""
 
 LOG4J=$DIR/log4j2.xml
-RTI_RID_FILE=$DIR/RTI.rid
+RTI_FILE=$DIR/RTI.rid
 
-num=$((1 + RANDOM % 1000))
-
+cd $DIR/src/house/HouseProject_generated/HouseProject-java-federates/HouseProject-impl-java/House/target
+cp $RTI_FILE ./RTI.rid
 if [ -z "$1" ]
 then
-	name=house$num
+    echo "Running house with randomly generated name..."
+    xterm -hold $logtofile -e "java -Dlog4j.configurationFile=$LOG4J -jar House-0.1.0-SNAPSHOT.jar -configFile=conf/HouseConfig.json" &
 else
-	name=$1
+    echo "Running house with the name $1..."
+    xterm -hold $logtofile -e "java -Dlog4j.configurationFile=$LOG4J -jar House-0.1.0-SNAPSHOT.jar -configFile=conf/HouseConfig.json -name=$1" &
 fi
-
-echo "running house federate name: $name" 
-cd $DIR/src/house/HouseProject_generated/HouseProject-java-federates/HouseProject-impl-java/House/target
-xterm -hold $logtofile -e "java -Dlog4j.configurationFile=$LOG4J -jar House-0.1.0-SNAPSHOT.jar -configFile=conf/HouseConfig.json -name=$name" &
-
-cd $DIR
 
