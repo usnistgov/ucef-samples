bea7c1075856edf86fc6b8c1a7e87348d35c0b11
Thomas Roth <thomas.roth@nist.gov>
2017-11-15 17:56:59 +0000

    create object instances

--- SensorAggregation/Environment/src/main/java/gov/nist/hla/samples/aggregator/Environment.java
+++ SensorAggregation/Environment/src/main/java/gov/nist/hla/samples/aggregator/Environment.java
@@ -3,7 +3,9 @@ package gov.nist.hla.samples.aggregator;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,11 +14,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.nist.hla.ii.InjectionCallback;
 import gov.nist.hla.ii.InjectionFederate;
+import hla.rti.FederateNotExecutionMember;
+import hla.rti.NameNotFound;
+import hla.rti.ObjectClassNotPublished;
 
 public class Environment implements InjectionCallback {
     private static final Logger log = LogManager.getLogger();
     
+    private static final String OBJECT_SPEED_SENSOR = "ObjectRoot.Sensor.SpeedSensor";
+    private static final String OBJECT_VOLUME_SENSOR = "ObjectRoot.Sensor.VolumeSensor";
+    
     private InjectionFederate gateway;
+    
+    private EnvironmentConfiguration configuration;
+    
+    private Set<String> speedSensors = new HashSet<String>();
+    private Set<String> volumeSensors = new HashSet<String>();
     
     public static void main(String[] args)
             throws IOException {
@@ -40,6 +53,7 @@ public class Environment implements InjectionCallback {
     
     public Environment(EnvironmentConfiguration configuration) {
         this.gateway = new InjectionFederate(configuration, this);
+        this.configuration = configuration;
     }
     
     public void run() {
@@ -57,13 +71,33 @@ public class Environment implements InjectionCallback {
     }
 
     public void initializeSelf() {
-        // TODO Auto-generated method stub
+        final int speedClusters = configuration.getNumberOfSpeedClusters();
+        log.info("creating " + speedClusters + " object instances of " + OBJECT_SPEED_SENSOR);
+        for (int i = 0; i < speedClusters; i++) {
+            try {
+                String name = gateway.registerObjectInstance(OBJECT_SPEED_SENSOR);
+                log.debug("\tnew instance with name " + name);
+                speedSensors.add(name);
+            } catch (FederateNotExecutionMember | NameNotFound | ObjectClassNotPublished e) {
+                throw new RuntimeException(e);
+            }
+        }
         
+        final int volumeClusters = configuration.getNumberOfVolumeClusters();
+        log.info("creating " + volumeClusters + " object instances of " + OBJECT_VOLUME_SENSOR);
+        for (int i = 0; i < volumeClusters; i++) {
+            try {
+                String name = gateway.registerObjectInstance(OBJECT_VOLUME_SENSOR);
+                log.debug("\tnew instance with name " + name);
+                volumeSensors.add(name);
+            } catch (FederateNotExecutionMember | NameNotFound | ObjectClassNotPublished e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void initializeWithPeers() {
-        // TODO Auto-generated method stub
-        
+        // maybe send interaction with expected number of sensors to aggregator
     }
 
     public void beforeTimeStep(Double timeStep) {
