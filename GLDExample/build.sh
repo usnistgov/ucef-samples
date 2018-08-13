93f59c0981602e5e15650d940917a0f1c6232cda
Thomas Roth <thomas.roth@nist.gov>
2018-08-13 20:12:32 +0000

    new gridlabd example

---
+++ GLDExample/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rootdir=`pwd`
+
+cd $rootdir/src/GLDExample_generated
+mvn clean install
+
+cd $rootdir/src/GLDExample_deployment
+mvn clean install
+
+cd $rootdir/src/GLDFederate
+mvn package
+
---
+++ GLDExample/src/GLDExample_generated/GLDExample-java-federates/GLDExample-impl-java/TestFederate/src/main/java/GLDExample/TestFederateConfig.java
@@ -0,0 +1,10 @@
+package GLDExample;
+
+import org.cpswt.config.FederateConfig;
+import org.cpswt.config.FederateParameter;
+
+public class TestFederateConfig extends FederateConfig {
+    @FederateParameter
+    public boolean useCelsius;
+}
+
