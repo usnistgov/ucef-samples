e60eab4a09c59be7b1f6ae072b6e2c1045d490d1
Thomas Roth <thomas.roth@nist.gov>
2018-09-04 19:36:41 +0000

    simulation time federate

--- GLDExample/src/GLDExample_generated/GLDExample-java-federates/GLDExample-impl-java/SimulationTime/src/main/java/GLDExample/SimulationTime.java
+++ GLDExample/src/GLDExample_generated/GLDExample-java-federates/GLDExample-impl-java/SimulationTime/src/main/java/GLDExample/SimulationTime.java
@@ -1,6 +1,5 @@
 package GLDExample;
 
-import org.cpswt.config.FederateConfig;
 import org.cpswt.config.FederateConfigParser;
 import org.cpswt.hla.base.AdvanceTimeRequest;
 
@@ -8,16 +7,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * The SimulationTime type of federate for the federation designed in WebGME.
- *
+ * A federate that provides the SimTime interaction prior to logical time progression.
  */
 public class SimulationTime extends SimulationTimeBase {
     private final static Logger log = LogManager.getLogger();
 
+    private SimulationTimeConfig configuration = null;
+    
     private double currentTime = 0;
 
-    public SimulationTime(FederateConfig params) throws Exception {
+    public SimulationTime(SimulationTimeConfig params) throws Exception {
         super(params);
+        this.configuration = params;
     }
 
     private void execute() throws Exception {
@@ -27,10 +28,6 @@ public class SimulationTime extends SimulationTimeBase {
             super.disableTimeRegulation();
         }
 
-        /////////////////////////////////////////////
-        // TODO perform basic initialization below //
-        /////////////////////////////////////////////
-
         AdvanceTimeRequest atr = new AdvanceTimeRequest(currentTime);
         putAdvanceTimeRequest(atr);
 
@@ -39,10 +36,13 @@ public class SimulationTime extends SimulationTimeBase {
             readyToPopulate();
             log.info("...synchronized on readyToPopulate");
         }
-
-        ///////////////////////////////////////////////////////////////////////
-        // TODO perform initialization that depends on other federates below //
-        ///////////////////////////////////////////////////////////////////////
+        
+        SimTime simTime = create_SimTime();
+        simTime.set_timeScale(configuration.timeScale);
+        simTime.set_timeZone(configuration.timeZone);
+        simTime.set_unixTimeStart(configuration.unixTimeStart);
+        simTime.set_unixTimeStop(configuration.unixTimeStop);
+        simTime.sendInteraction(getLRC()); // send RO
 
         if(!super.isLateJoiner()) {
             log.info("waiting on readyToRun...");
@@ -57,28 +57,6 @@ public class SimulationTime extends SimulationTimeBase {
             atr.requestSyncStart();
             enteredTimeGrantedState();
 
-            ////////////////////////////////////////////////////////////////////////////////////////
-            // TODO send interactions that must be sent every logical time step below.
-            // Set the interaction's parameters.
-            //
-            //    SimTime vSimTime = create_SimTime();
-            //    vSimTime.set_actualLogicalGenerationTime( < YOUR VALUE HERE > );
-            //    vSimTime.set_federateFilter( < YOUR VALUE HERE > );
-            //    vSimTime.set_originFed( < YOUR VALUE HERE > );
-            //    vSimTime.set_sourceFed( < YOUR VALUE HERE > );
-            //    vSimTime.set_timeScale( < YOUR VALUE HERE > );
-            //    vSimTime.set_timeZone( < YOUR VALUE HERE > );
-            //    vSimTime.set_unixTimeStart( < YOUR VALUE HERE > );
-            //    vSimTime.set_unixTimeStop( < YOUR VALUE HERE > );
-            //    vSimTime.sendInteraction(getLRC(), currentTime + getLookAhead());
-            //
-            ////////////////////////////////////////////////////////////////////////////////////////
-
-            ////////////////////////////////////////////////////////////////////////////////////////
-            // TODO break here if ready to resign and break out of while loop
-            ////////////////////////////////////////////////////////////////////////////////////////
-
-
             if (!exitCondition) {
                 currentTime += super.getStepSize();
                 AdvanceTimeRequest newATR = new AdvanceTimeRequest(currentTime);
@@ -90,16 +68,12 @@ public class SimulationTime extends SimulationTimeBase {
 
         // call exitGracefully to shut down federate
         exitGracefully();
-
-        ////////////////////////////////////////////////////////////////////////////////////////
-        // TODO Perform whatever cleanups needed before exiting the app
-        ////////////////////////////////////////////////////////////////////////////////////////
     }
 
     public static void main(String[] args) {
         try {
             FederateConfigParser federateConfigParser = new FederateConfigParser();
-            FederateConfig federateConfig = federateConfigParser.parseArgs(args, FederateConfig.class);
+            SimulationTimeConfig federateConfig = federateConfigParser.parseArgs(args, SimulationTimeConfig.class);
             SimulationTime federate = new SimulationTime(federateConfig);
             federate.execute();
             log.info("Done.");
