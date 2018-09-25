67978ac6bfb5ff43dd97bf45c00426b5b8555f69
Thomas Roth <thomas.roth@nist.gov>
2018-09-25 21:15:54 +0000

    ChallengeResponse comments for java code

--- ChallengeResponse/src/ChallengeResponse_generated/ChallengeResponse-java-federates/ChallengeResponse-impl-java/JavaChallenger/src/main/java/ChallengeResponse/Configuration.java
+++ ChallengeResponse/src/ChallengeResponse_generated/ChallengeResponse-java-federates/ChallengeResponse-impl-java/JavaChallenger/src/main/java/ChallengeResponse/Configuration.java
@@ -3,10 +3,19 @@ package ChallengeResponse;
 import org.cpswt.config.FederateConfig;
 import org.cpswt.config.FederateParameter;
 
+/*
+ * A custom configuration file for the challenger federate.
+ */
 public class Configuration extends FederateConfig {
+    /*
+     * The number of characters to generate for the challenge string.
+     */
     @FederateParameter
     public int challengeLength;
     
+    /*
+     * The number of responses that should be received for each challenge.
+     */
     @FederateParameter
     public int numberOfResponders;
 }
--- ChallengeResponse/src/ChallengeResponse_generated/ChallengeResponse-java-federates/ChallengeResponse-impl-java/JavaChallenger/src/main/java/ChallengeResponse/ResponseInfo.java
+++ ChallengeResponse/src/ChallengeResponse_generated/ChallengeResponse-java-federates/ChallengeResponse-impl-java/JavaChallenger/src/main/java/ChallengeResponse/ResponseInfo.java
@@ -2,13 +2,30 @@ package ChallengeResponse;
 
 import java.util.Objects;
 
+/*
+ * A simple data structure that stores a single response to a challenge.
+ * 
+ * Two responses are equivalent iff they refer to the same challenge id and come from the same responder.
+ */
 public class ResponseInfo {
+    /*
+     * The unique identifier of the associated challenge.
+     */
     private final String id;
     
+    /*
+     * The response to the challenge.
+     */
     private final String substring;
     
+    /*
+     * The federate that sent the response.
+     */
     private final String responder;
     
+    /*
+     * The logical time when the response was received.
+     */
     private final double responseTime;
     
     public ResponseInfo(String id, String substring, String responder, double responseTime) {
