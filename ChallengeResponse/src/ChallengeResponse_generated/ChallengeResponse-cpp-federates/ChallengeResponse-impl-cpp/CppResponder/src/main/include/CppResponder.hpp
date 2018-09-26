acf6c28475b1c4e340fbe9e6a6db9f4a8e03a72e
Thomas Roth <thomas.roth@nist.gov>
2018-09-26 19:18:17 +0000

    ChallengeResponse comments for c++ code

--- ChallengeResponse/src/ChallengeResponse_generated/ChallengeResponse-cpp-federates/ChallengeResponse-impl-cpp/CppResponder/src/main/include/CppResponder.hpp
+++ ChallengeResponse/src/ChallengeResponse_generated/ChallengeResponse-cpp-federates/ChallengeResponse-impl-cpp/CppResponder/src/main/include/CppResponder.hpp
@@ -5,17 +5,36 @@
 #include "FederateConfigParser.h"
 #include "FederateConfig.h"
 
-
+/*
+ * This class broadcasts response interactions to the federation each time it receives a challenge.
+ *
+ * A challenge is a randomly generated string paired with an integer index of one of the elements. A correct response
+ * for a given challenge is the substring that starts from the challenge index. To generate the correct response, a
+ * federate must understand both the string and numeric data in the challenge.
+ *
+ * This class generates and sends a response to challenges received as either as an interaction or an object update. It
+ * is stateless, and will generate one response per received challenge.
+ */
 class CppResponder : public CppResponderBase {
     private:
         double m_currentTime;
 
+        /*
+         * Handle and respond to challenges sent as object updates.
+         */
         void handleObjectClass(boost::shared_ptr<ChallengeObject> object);
 
+        /*
+         * Handle and respond to challenges sent as interactions.
+         */
         void handleInteractionClass(boost::shared_ptr<ChallengeInteraction> interaction);
 
         void checkReceivedSubscriptions();
 
+        /*
+         * Send a response interaction for the given challenge. The response content will be set to the substring of the
+         * given string starting from the given index.
+         */
         void respond(const std::string &id, const std::string &stringValue, int beginIndex);
 
     public:
